784d82e8950a27ac2d550761085b88b2a338a4a3
radnan <rafiadnan47@gmail.com>
2014-09-11 19:21:56 +0600

    add tests for merge request #3

--- tests/RdnCsv/Controller/Plugin/CsvExportTest.php
+++ tests/RdnCsv/Controller/Plugin/CsvExportTest.php
@@ -10,6 +10,24 @@ class CsvExportTest extends \PHPUnit_Framework_TestCase
 {
 	private $vfs;
 
+	private $header = array(
+		'Year',
+		'Make',
+		'Model',
+		'Description',
+		'Price',
+	);
+
+	private $records = array(
+		array(
+			'1997',
+			'Ford',
+			'E350',
+			'ac, abs, moon',
+			'3000.00',
+		),
+	);
+
 	public function setUp()
 	{
 		$this->vfs = vfsStream::setup('root');
@@ -18,23 +36,7 @@ class CsvExportTest extends \PHPUnit_Framework_TestCase
 	public function testExport()
 	{
 		$csv = new CsvExport;
-		$header = array(
-			'Year',
-			'Make',
-			'Model',
-			'Description',
-			'Price',
-		);
-		$records = array(
-			array(
-				'1997',
-				'Ford',
-				'E350',
-				'ac, abs, moon',
-				'3000.00',
-			),
-		);
-		$response = $csv->__invoke('foo.csv', $header, $records);
+		$response = $csv->__invoke('foo.csv', $this->header, $this->records);
 
 		$type = $response->getHeaders()->get('Content-Type')->getFieldValue();
 		$this->assertEquals('text/csv', $type);
@@ -45,6 +47,19 @@ class CsvExportTest extends \PHPUnit_Framework_TestCase
 		$content = <<<CSV
 Year,Make,Model,Description,Price
 1997,Ford,E350,"ac, abs, moon",3000.00
+
+CSV;
+		$this->assertEquals($content, $response->getContent());
+	}
+
+	public function testExportCustomControls()
+	{
+		$csv = new CsvExport;
+		$response = $csv->__invoke('foo.csv', $this->header, $this->records, null, ';', "'");
+
+		$content = <<<CSV
+Year,Make,Model,Description,Price
+1997;Ford;E350;'ac, abs, moon';3000.00
 
 CSV;
 		$this->assertEquals($content, $response->getContent());
